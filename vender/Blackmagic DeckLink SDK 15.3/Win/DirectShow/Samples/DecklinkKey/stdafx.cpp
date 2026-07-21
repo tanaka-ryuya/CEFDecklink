d27e2876794b0017473668656bf754c487e0370f
tanaka-ryuya <ryuyaicehockey@gmail.com>
2026-07-21 10:24:59 +0900

    chore: prepare repository for public release (remove binary/sensitive files, configure CI)

--- vender/Blackmagic DeckLink SDK 15.3/Win/DirectShow/Samples/DecklinkKey/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// DecklinkKey.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
